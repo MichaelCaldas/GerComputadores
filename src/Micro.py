1fe21ced258567e6eed428a26f3c0bd7a0928a43
MichaelCaldas <michael.caldas.silva@gmail.com>
2011-02-15 01:56:06 -0200

    Atualização das classes Micro e Teste_Micro.

---
+++ src/Micro.py
@@ -0,0 +1,17 @@
+'''
+Created on 13/02/2011
+
+@author: Michael
+'''
+class Micro():  
+    todosMicros = []
+    
+    def __init__(self, codPatrimonio, descricao, hd, memoria):
+        self.codPatrimonio = codPatrimonio
+        self.descricao = descricao
+        self.hd = hd
+        self.memoria = memoria
+        self.armazenaMicros(self)
+    
+    def armazenaMicros(self, micro):
+        self.todosMicros.append(micro)
--- src/Teste_Micro.py
+++ src/Teste_Micro.py
@@ -3,16 +3,21 @@ Created on 13/02/2011
 
 @author: Michael
 '''
-import unittest
 from Micro import Micro
 from should_dsl.dsl import should
-from should_dsl.matchers import be
+from should_dsl.matchers import be, equal_to
+import unittest
 
-class Test_Micro(unittest.TestCase):
+class Teste_Micro(unittest.TestCase):
 
-    def setUp(self):
+    def testeAdicionaUmMicro(self):
         self.micro = Micro("001", "Micro ultra veloz", 500, 4)
         self.micro.codPatrimonio |should| be("001")
+        self.micro.descricao |should| equal_to("Micro ultra veloz")
+        self.micro.hd |should| equal_to(500)
+        self.micro.memoria |should| equal_to(4)
+        self.micro.todosMicros[0].codPatrimonio |should| equal_to("001")
+        
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
